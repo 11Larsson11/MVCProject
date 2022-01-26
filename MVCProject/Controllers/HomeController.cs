3d582b6b47a4cd1a8cf4ac604dd113a2672ec084
11Larsson11 <richard.auerhahn@gmail.com>
2022-01-26 11:28:07 +0100

    Add project files.

---
+++ MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.IntroMessage = "Hello and welcome!";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.ContactMessage = "You can contact mr. Larsson via mail:";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+
+            ViewBag.Message = "I’m a creative product developer with a passion for finding solutions to problems. I have worked in a wide range of projects, from the car industry to app development. As a person, I am creative, analytical, and open-minded.";
+
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            ViewBag.ProjectMessage = "More projects are incoming.";
+
+            return View();
+        }
+    }
+}
